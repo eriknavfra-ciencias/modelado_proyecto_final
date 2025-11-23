cd84baef3462f57f334e3b4bf61d6f20717a9c31
Renata Feregrino <renata.mesinas@ciencias.unam.mx>
2025-11-22 18:00:24 -0600

    forms & db

--- proyecto_final/agenda/forms.py
+++ proyecto_final/agenda/forms.py
@@ -5,4 +5,9 @@ from .models import Tarea
 class TareaForm(forms.ModelForm):
     class Meta:
         model = Tarea
-        fields = ["titulo", "prioridad", "fecha", "descripcion", "etiquetas", "completada"]
+        fields = ["titulo", "prioridad", "fecha", "descripcion", "etiquetas"]
+
+        widgets = {
+            'fecha': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'rows': 3}),
+        }
--- proyecto_final/agenda/views.py
+++ proyecto_final/agenda/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Tarea
 from .forms import TareaForm
+from django.shortcuts import get_object_or_404
 
 def lista_tareas(request):
     tareas = Tarea.objects.all()
@@ -15,4 +16,15 @@ def nueva_tarea(request):
     else:
         form = TareaForm()
     return render(request, "agenda/nueva.html", {"form": form})
+
+def completar_tarea(request, tarea_id):
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    tarea.completada = True
+    tarea.save()
+    return redirect("lista_tareas")
+
+def eliminar_tarea(request, tarea_id):
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    tarea.delete()
+    return redirect("lista_tareas")
 
